499ccad1999ca52f0d6d7a20fcfd7a2eddb73cb7
Pavithran Lallu <pavithranlallu@Pavithran-MacBook-Air.local>
2025-09-05 19:10:24 +0530

    env added

--- src/app.js
+++ src/app.js
@@ -2,14 +2,19 @@ import express from "express";
 import userRoutes from "./routes/userRoutes.js";
 import authRoutes from "./routes/authRoutes.js"
 import cors from 'cors';
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
-
 //front end and backend connection
 app.use(cors({
-  origin: 'http://localhost:4200', // Angular dev server
+  origin: process.env.NODE_ENV === 'production' 
+    ? process.env.FRONTEND_URL 
+    : ['http://localhost:4200', process.env.FRONTEND_URL],
   credentials: true
-}));
+})
+
+);
 
 // Middleware
 app.use(express.json());
